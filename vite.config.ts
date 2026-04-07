51c3e8460ccb614a4c89fc6831c1d8816e598188
Vince Iver <vince.iver17@gmail.com>
2026-04-07 13:57:32 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-const base = process.env.GITHUB_ACTIONS === 'true' && repoName
-  ? `/${repoName}/`
-  : '/'
-
 // https://vite.dev/config/
 export default defineConfig({
-  base,
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
